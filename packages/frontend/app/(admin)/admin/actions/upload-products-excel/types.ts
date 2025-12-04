15eab5661e64b1ff424863efe4f444cf20d63521
smsarov <smsarov@gmail.com>
2025-12-04 22:32:29 +0300

    2

--- packages/frontend/app/(admin)/admin/actions/upload-products-excel/types.ts
+++
@@ -1,9 +0,0 @@
-import { products } from "@db/schema"
-import type { Insertable, InsertableDb, Selectable } from "@db/types/utils"
-
-
-export type InsertableProduct = Insertable<typeof products>;
-export type InsertableProductDb = InsertableDb<typeof products>;
-export type SelectedProduct = Selectable<typeof products>;
-
-export type ParsedProduct = Omit<InsertableProduct, 'productId'>
